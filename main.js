fffd8fc9d623b3e79d343ab48159520a7b0f19dc
Mohammed Hussein <mohammedhussein004m@gmail.com>
2024-11-13 12:30:25 +0200

    add

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+// Function to copy text
+// Get the input and button elements
+let input = document.getElementById("input");
+let button = document.getElementById("button");
 function copy() {
-  let input = document.getElementById("input");
-  let button = document.getElementById("button");
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(input.value);
+  } else {
+    // Fallback method for older devices
+    input.select();
+    document.execCommand("copy");
+  }
 
-  navigator.clipboard.writeText(input.value);
   button.textContent = "Copied";
 }
